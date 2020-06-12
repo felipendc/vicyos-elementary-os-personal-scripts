699f11c47044c21be6efe35fcfec703640d564ce
felipendc <felipendc10@gmail.com>
2020-06-11 22:40:46 -0300

    Vicyos Rolling Update: (qui jun 11 22:40:46 -03 2020)

--- vicyos-optional-bashrc-and-zshrc/installation-files/vicyos-zsh-bash-install.sh
+++ vicyos-optional-bashrc-and-zshrc/installation-files/vicyos-zsh-bash-install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 # Install zsh and personal pkgs 
-sudo apt install -y git zsh neofetch adb fastboot
-
+sudo apt install -y git zsh neofetch
 
 # Install oh-my-zsh
 curl -Lo install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 sh install.sh
 
-
-# Move folders and files to ~/
+# Move folders and files to home folder
 cp -r .vicyos-zsh-plugins ~/
-cp -r .bashrc ~/
 cp -r .zshrc ~/
+
+# Use the my moded .bashrc file
+cd ../../ && cp -r elementary-os-personal-bashrc/.bashrc ~/
+
+# Add zsh to .bashrc file to call auto zsh on terminal
+echo "zsh" >> ~/.bashrc
 
 
 # cleanup
+cd vicyos-optional-bashrc-and-zshrc/installation-files/
 sudo rm -r install.sh
