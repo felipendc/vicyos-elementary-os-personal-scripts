da286dde136a3872d4425a4ad7158301359f2f8d
felipendc <felipendc10@gmail.com>
2021-08-12 09:43:05 -0300

    up

--- upall/setup-git-v1.sh
+++ upall/setup-git-v1.sh
@@ -3,16 +3,15 @@
 # github.com/felipendc
 
 # Go back to the root (main) folder
-cd ../
+# cd ../
 
-git init
+# git init
 git config --global user.name "felipendc"
 git config --global user.email "felipendc10@gmail.com"
-sudo git config --system core.editor nano
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=25000'
-git config --global push.default simple
-
+# sudo git config --system core.editor nano
+# git config --global credential.helper cache
+# git config --global credential.helper 'cache --timeout=25000'
+# git config --global push.default simple
 
 echo "################################################################"
 echo "###################    T H E   E N D      ######################"
